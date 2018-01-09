ae6c66da2df05dbfda766e77440ebe675dccb6e0
Dave Snowberger <git@fartherout.org>
2018-01-08 17:45:44 -0800

    did the kata

--- kata.js
+++ kata.js
@@ -1,3 +1,3 @@
-function abbrevName(name){
-  return name.split(' ').reduce(function(a,b) {return a + b[0] + '.'},'').toUpperCase().slice(0,3);
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
 }
