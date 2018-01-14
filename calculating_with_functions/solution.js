e37b43517c9c29f889ed9eb22844a4d29a8b2c6d
Dave Snowberger <git@fartherout.org>
2018-01-13 20:03:38 -0800

    solved kata

---
+++ calculating_with_functions/solution.js
@@ -0,0 +1,15 @@
+var zero = right => right ? eval('0' + right.toString()) : 0;
+var one = right => 1 + right ? eval('1' + right.toString()) : 1;
+var two = right => 2 + right ? eval('2' + right.toString()) : 2;
+var three = right => 3 + right ? eval('3' + right.toString()) : 3;
+var four = right => 4 + right ? eval('4' + right.toString()) : 4;
+var five = right => 5 + right ? eval('5' + right.toString()) : 5;
+var six = right => 6 + right ? eval('6' + right.toString()) : 6;
+var seven = right => 7 + right ? eval('7' + right.toString()) : 7;
+var eight = right => 8 + right ? eval('8' + right.toString()) : 8;
+var nine = right => 9 + right ? eval('9' + right.toString()) : 9;
+
+var plus = num => '+' + num.toString();
+var minus = num => '-' + num.toString();
+var times = num => '*' + num.toString();
+var dividedBy = num => '/' + num.toString();
